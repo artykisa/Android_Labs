4a799cbc6ab1af57fa474366e8b093ce7ca5ff09
artykisa <crazypluton@gmail.com>
2020-09-21 00:40:44 +0300

    start view

--- lab1/app/src/main/java/com/example/lab1/MainActivity.kt
+++ lab1/app/src/main/java/com/example/lab1/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.lab1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button_result.setOnClickListener {
+            resultText.text = "Hello Kitty!"
+        }
     }
 }
