9836044b1da4052503858d6af58d4c114ee5f47f
artykisa <crazypluton@gmail.com>
2020-09-21 01:11:23 +0300

    added buttons

--- lab1/app/src/main/java/com/example/lab1/MainActivity.kt
+++ lab1/app/src/main/java/com/example/lab1/MainActivity.kt
@@ -9,8 +9,52 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        button_result.setOnClickListener {
-            resultText.text = "Hello Kitty!"
+        button_1.setOnClickListener {
+            resultText.text = "1"
+        }
+
+        button_2.setOnClickListener {
+            resultText.text = "2"
+        }
+
+        button_3.setOnClickListener {
+            resultText.text = "3"
+        }
+
+        button_4.setOnClickListener {
+            resultText.text = "4"
+        }
+
+        button_5.setOnClickListener {
+            resultText.text = "5"
+        }
+
+        button_6.setOnClickListener {
+            resultText.text = "6"
+        }
+
+        button_7.setOnClickListener {
+            resultText.text = "7"
+        }
+
+        button_8.setOnClickListener {
+            resultText.text = "8"
+        }
+
+        button_9.setOnClickListener {
+            resultText.text = "9"
+        }
+
+        button_0.setOnClickListener {
+            resultText.text = "0"
+        }
+
+        button_x.setOnClickListener {
+            resultText.text = ""
+        }
+
+        button_dot.setOnClickListener {
+            resultText.text = "."
         }
     }
 }
