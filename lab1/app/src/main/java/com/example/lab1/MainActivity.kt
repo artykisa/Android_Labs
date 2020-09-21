74621bcb40a80b1443136b948c6ad0c329add5e4
artykisa <crazypluton@gmail.com>
2020-09-21 12:39:42 +0300

    added full num-pad

--- lab1/app/src/main/java/com/example/lab1/MainActivity.kt
+++ lab1/app/src/main/java/com/example/lab1/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.lab1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -9,52 +10,73 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //distance, weight, currency
+        val distance: List<String> = listOf("kilometer","meter","centimeter")
+        val weight: List<String> = listOf("ton","kilogram","gram")
+        val currency: List<String> = listOf("BYN","DOLLAR","EURO")
+
+        fun set_text(x: String) {
+            val sb = StringBuilder()
+            val myString = result_Text.text
+            if(myString.length!=12) {
+                sb.append(myString).append(x)
+                result_Text.text = sb
+            }
+            else {
+                val text = "Max length!"
+                val duration = Toast.LENGTH_SHORT
+                val toast = Toast.makeText(applicationContext, text, duration)
+                toast.show()
+            }
+        }
+
         button_1.setOnClickListener {
-            resultText.text = "1"
+            set_text("1")
         }
 
         button_2.setOnClickListener {
-            resultText.text = "2"
+            set_text("2")
         }
 
         button_3.setOnClickListener {
-            resultText.text = "3"
+            set_text("3")
         }
 
         button_4.setOnClickListener {
-            resultText.text = "4"
+            set_text("4")
         }
 
         button_5.setOnClickListener {
-            resultText.text = "5"
+            set_text("5")
         }
 
         button_6.setOnClickListener {
-            resultText.text = "6"
+            set_text("6")
         }
 
         button_7.setOnClickListener {
-            resultText.text = "7"
+            set_text("7")
         }
 
         button_8.setOnClickListener {
-            resultText.text = "8"
+            set_text("8")
         }
 
         button_9.setOnClickListener {
-            resultText.text = "9"
+            set_text("9")
         }
 
         button_0.setOnClickListener {
-            resultText.text = "0"
+            set_text("0")
         }
 
         button_x.setOnClickListener {
-            resultText.text = ""
+            val myString = result_Text.text
+            result_Text.text = myString.dropLast(1)
         }
 
         button_dot.setOnClickListener {
-            resultText.text = "."
+            set_text(".")
         }
     }
 }
