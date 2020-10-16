9fb774926209e9e42f98f03b492e8cf759f6cd1a
artykisa <crazypluton@gmail.com>
2020-10-16 13:40:46 +0300

    add

--- lab1/app/src/main/java/com/example/lab1/MainActivity.kt
+++ lab1/app/src/main/java/com/example/lab1/MainActivity.kt
@@ -2,217 +2,61 @@ package com.example.lab1
 
 
 import android.os.Bundle
-import android.view.View
-import android.widget.AdapterView
-import android.widget.AdapterView.OnItemSelectedListener
 import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_view.*
 
 
 
 class MainActivity : AppCompatActivity() {
+    fun get_adapter_weight(): ArrayAdapter<String> {
+        val WeightList = resources.getStringArray(R.array.Weight)
+        val adapter_weight = ArrayAdapter(
+            this@MainActivity,
+            android.R.layout.simple_spinner_item, WeightList
+        )
+        return adapter_weight
+    }
+    fun get_adapter_currency(): ArrayAdapter<String> {
+        val CurencyList = resources.getStringArray(R.array.Currency)
+        val adapter_curency = ArrayAdapter(
+            this@MainActivity,
+            android.R.layout.simple_spinner_item, CurencyList
+        )
+        return adapter_curency
+    }
+    fun get_adapter_distance(): ArrayAdapter<String> {
+        val DistanceList = resources.getStringArray(R.array.Distance)
+        val adapter_distance = ArrayAdapter(
+            this@MainActivity,
+            android.R.layout.simple_spinner_item, DistanceList
+        )
+        return adapter_distance
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
 
-        /*distance, weight, currency
-        val distance: List<String> = listOf("kilometer","meter","centimeter")
-        val weight: List<String> = listOf("ton","kilogram","gram")
-        val currency: List<String> = listOf("BYN","DOLLAR","EURO")*/
-
-        /*fun set_text(x: String) {
-            val sb = StringBuilder()
-            val myString = result_Text.text
-            if(myString.length!=12) {
-                sb.append(myString).append(x)
-                result_Text.text = sb
-            }
-            else {
-                val text = "Max length!"
-                val duration = Toast.LENGTH_SHORT
-                val toast = Toast.makeText(applicationContext, text, duration)
-                toast.show()
-            }
-        }*/
-
-        button_change.setOnClickListener {
-            var text_string_from = result_Text.text
-            var text_string_to = result_Text2.text
-            result_Text.text = text_string_to
-            result_Text2.text = text_string_from
-            // add for spinner
+    fun set_text(x: String) {
+        val sb = StringBuilder()
+        val myString = fragment.result_Text.text
+        if(myString.length!=12) {
+            sb.append(myString).append(x)
+            fragment.result_Text.text = sb
         }
-
-        button_result.setOnClickListener {
-            var text_to_transform = result_Text.text.toString()
-            if(text_to_transform==""){
-                val text = "No data!"
-                val duration = Toast.LENGTH_SHORT
-                val toast = Toast.makeText(applicationContext, text, duration)
-                toast.show()
-                result_Text2.text=""
-            }
-            else {
-                var int_value = text_to_transform.toDouble()
-                if(spinner_value.selectedItem.toString()=="Weight"){
-                    if(spinner_choose2.selectedItem.toString()=="Gram"){
-                        if(spinner_choose.selectedItem.toString()=="Kilogram"){
-                            int_value /= 1000
-                        }
-                        else if(spinner_choose.selectedItem.toString()=="Gram"){
-                            int_value = int_value
-                        }
-                        else{
-                            int_value /= 1000000
-                        }
-                    }
-                    else if(spinner_choose2.selectedItem.toString()=="Kilogram"){
-                        if(spinner_choose.selectedItem.toString()=="Kilogram"){
-                            int_value = int_value
-                        }
-                        else if(spinner_choose.selectedItem.toString()=="Gram"){
-                            int_value = int_value * 1000
-                        }
-                        else{
-                            int_value /= 1000
-                        }
-                    }
-                    else{
-                        if(spinner_choose.selectedItem.toString()=="Kilogram"){
-                            int_value /= 1000
-                        }
-                        else if(spinner_choose.selectedItem.toString()=="Gram"){
-                            int_value /= 1000000
-                        }
-                        else{
-                            int_value = int_value
-                        }
-                    }
-                }
-                else if(spinner_value.selectedItem.toString()=="Distance"){
-                        if(spinner_choose2.selectedItem.toString()=="Centimeter"){
-                            if(spinner_choose.selectedItem.toString()=="Meter"){
-                                int_value /= 100
-                            }
-                            else if(spinner_choose.selectedItem.toString()=="Centimeter"){
-                                int_value = int_value
-                            }
-                            else{
-                                int_value /= 100000
-                            }
-                        }
-                        else if(spinner_choose2.selectedItem.toString()=="Meter"){
-                            if(spinner_choose.selectedItem.toString()=="Meter"){
-                                int_value = int_value
-                            }
-                            else if(spinner_choose.selectedItem.toString()=="Centimeter"){
-                                int_value = int_value * 100
-                            }
-                            else{
-                                int_value /= 1000
-                            }
-                        }
-                    else{
-                            if(spinner_choose.selectedItem.toString()=="Meter"){
-                                int_value *= 1000
-                            }
-                            else if(spinner_choose.selectedItem.toString()=="Centimeter"){
-                                int_value *= 100000
-                            }
-                            else{
-                                int_value = int_value
-                            }
-                        }
-                }
-                else {
-                    if (spinner_choose2.selectedItem.toString() == "BYN") {
-                        if (spinner_choose.selectedItem.toString() == "BYN") {
-                            int_value = int_value
-                        } else if (spinner_choose.selectedItem.toString() == "Dollar") {
-                            int_value /= 2.6
-                        } else {
-                            int_value /= 3.17
-                        }
-                    }
-                    else if (spinner_choose2.selectedItem.toString() == "Dollar") {
-                        if (spinner_choose.selectedItem.toString() == "BYN") {
-                            int_value *= 2.6
-                        }
-                        else if (spinner_choose.selectedItem.toString() == "Dollar") {
-                            int_value = int_value
-                        }
-                        else {
-                            int_value /= 1.17
-                        }
-                    }
-                    else{
-                        if (spinner_choose.selectedItem.toString() == "BYN") {
-                            int_value *= 3.1
-                        }
-                        else if (spinner_choose.selectedItem.toString() == "Dollar") {
-                            int_value = int_value * 1.17
-                        }
-                        else {
-                            int_value = int_value
-                        }
-                    }
-                }
-
-                result_Text2.text = int_value.toString()
-            }
+        else {
+            val text = "Max length!"
+            val duration = Toast.LENGTH_SHORT
+            val toast = Toast.makeText(applicationContext, text, duration)
+            toast.show()
         }
-
-
-        spinner_value.onItemSelectedListener = object : OnItemSelectedListener {
-            override fun onItemSelected(
-                parentView: AdapterView<*>?,
-                selectedItemView: View,
-                position: Int,
-                id: Long
-            ) {
-                if(spinner_value.selectedItem.toString()=="Weight"){
-                    val WeightList = resources.getStringArray(R.array.Weight)
-                    val adapter_weight = ArrayAdapter(
-                        this@MainActivity,
-                        android.R.layout.simple_spinner_item, WeightList
-                    )
-                    spinner_choose.adapter=adapter_weight
-                    spinner_choose2.adapter=adapter_weight
-                }
-                else if(spinner_value.selectedItem.toString()=="Distance"){
-                    val DistanceList = resources.getStringArray(R.array.Distance)
-                    val adapter_distance = ArrayAdapter(
-                        this@MainActivity,
-                        android.R.layout.simple_spinner_item, DistanceList
-                    )
-                    spinner_choose.adapter=adapter_distance
-                    spinner_choose2.adapter=adapter_distance
-                }
-                else{
-                    val CurencyList = resources.getStringArray(R.array.Currency)
-                    val adapter_curency = ArrayAdapter(
-                        this@MainActivity,
-                        android.R.layout.simple_spinner_item, CurencyList
-                    )
-                    spinner_choose.adapter=adapter_curency
-                    spinner_choose2.adapter=adapter_curency
-                }
-            }
-
-            override fun onNothingSelected(parentView: AdapterView<*>?) {
-                result_Text2.text="JOPA"
-            }
-        }
-
-        /*spinner_value.setOnClickListener(){
-            resultText.text=spinner_value[0].toString()
-        }*/
     }
     fun dot_press(){
-        val myString = result_Text.text
+        val myString = fragment.result_Text.text
         var dot_counter = 0
         if(myString.length==0){
             val text = "Dot can't be the first symbol!"
@@ -236,24 +80,8 @@ class MainActivity : AppCompatActivity() {
         }
         set_text(".")
     }
-
-    fun set_text(x: String) {
-        val sb = StringBuilder()
-        val myString = result_Text.text
-        if(myString.length!=12) {
-            sb.append(myString).append(x)
-            result_Text.text = sb
-        }
-        else {
-            val text = "Max length!"
-            val duration = Toast.LENGTH_SHORT
-            val toast = Toast.makeText(applicationContext, text, duration)
-            toast.show()
-        }
-    }
-
     fun x_press() {
-        val myString = result_Text.text
-        result_Text.text = myString.dropLast(1)
+        val myString = fragment.result_Text.text
+        fragment.result_Text.text = myString.dropLast(1)
     }
 }
